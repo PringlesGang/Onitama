4df6536fa5787bc4cb614b8c475daebfdebcb633
PringlesGang <liamelderbroek123@gmail.com>
2025-03-03 16:26:06 +0100

    Moved card mappings into std::unordered_maps

--- src/game/card.cpp
+++ src/game/card.cpp
@@ -1,128 +1,10 @@
 #include "card.h"
 
 #include <algorithm>
-#include <format>
-#include <stdexcept>
-#include <string>
 
 #include "../util/ansiColor.h"
 
 namespace Game {
-
-std::vector<Offset> Card::GetMoves(CardType card) {
-  switch (card) {
-    case CardType::Boar:
-      return std::vector({Offset(-1, 0), Offset(0, -1), Offset(1, 0)});
-    case CardType::Cobra:
-      return std::vector({Offset(-1, 0), Offset(1, -1), Offset(1, 1)});
-    case CardType::Crab:
-      return std::vector({Offset(-2, 0), Offset(0, -1), Offset(2, 0)});
-    case CardType::Crane:
-      return std::vector({Offset(-1, 1), Offset(0, -1), Offset(1, 1)});
-    case CardType::Dragon:
-      return std::vector(
-          {Offset(-2, -1), Offset(-1, 1), Offset(1, 1), Offset(2, -1)});
-    case CardType::Eel:
-      return std::vector({Offset(-1, -1), Offset(-1, 1), Offset(1, 0)});
-    case CardType::Elephant:
-      return std::vector(
-          {Offset(-1, -1), Offset(-1, 0), Offset(1, -1), Offset(1, 0)});
-    case CardType::Frog:
-      return std::vector({Offset(-2, 0), Offset(-1, -1), Offset(1, 1)});
-    case CardType::Goose:
-      return std::vector(
-          {Offset(-1, -1), Offset(-1, 0), Offset(1, 0), Offset(1, 1)});
-    case CardType::Horse:
-      return std::vector({Offset(-1, 0), Offset(0, -1), Offset(0, 1)});
-    case CardType::Mantis:
-      return std::vector({Offset(-1, -1), Offset(0, 1), Offset(1, -1)});
-    case CardType::Monkey:
-      return std::vector(
-          {Offset(-1, -1), Offset(-1, 1), Offset(1, -1), Offset(1, 1)});
-    case CardType::Ox:
-      return std::vector({Offset(0, -1), Offset(0, 1), Offset(1, 0)});
-    case CardType::Rabbit:
-      return std::vector({Offset(-1, 1), Offset(1, -1), Offset(2, 0)});
-    case CardType::Rooster:
-      return std::vector(
-          {Offset(-1, 0), Offset(-1, 1), Offset(1, 0), Offset(1, -1)});
-    case CardType::Tiger:
-      return std::vector({Offset(0, -2), Offset(0, 1)});
-
-    default:
-      const size_t cardNum = (size_t)card;
-      throw std::runtime_error(std::format("Invalid card type {}", cardNum));
-  }
-}
-
-Color Card::GetColor(CardType card) {
-  switch (card) {
-    case CardType::Boar:
-    case CardType::Cobra:
-    case CardType::Dragon:
-    case CardType::Elephant:
-    case CardType::Frog:
-    case CardType::Horse:
-    case CardType::Mantis:
-    case CardType::Rooster:
-      return Color::Red;
-
-    case CardType::Crab:
-    case CardType::Crane:
-    case CardType::Eel:
-    case CardType::Goose:
-    case CardType::Monkey:
-    case CardType::Ox:
-    case CardType::Rabbit:
-    case CardType::Tiger:
-      return Color::Blue;
-
-    default:
-      const size_t cardNum = (size_t)card;
-      throw std::runtime_error(std::format("Invalid card type {}", cardNum));
-  }
-}
-
-std::string Card::GetName(CardType card) {
-  switch (card) {
-    case CardType::Boar:
-      return "boar";
-    case CardType::Cobra:
-      return "cobra";
-    case CardType::Crab:
-      return "crab";
-    case CardType::Crane:
-      return "crane";
-    case CardType::Dragon:
-      return "dragon";
-    case CardType::Eel:
-      return "eel";
-    case CardType::Elephant:
-      return "elephant";
-    case CardType::Frog:
-      return "frog";
-    case CardType::Goose:
-      return "goose";
-    case CardType::Horse:
-      return "horse";
-    case CardType::Mantis:
-      return "mantis";
-    case CardType::Monkey:
-      return "monkey";
-    case CardType::Ox:
-      return "ox";
-    case CardType::Rabbit:
-      return "rabbit";
-    case CardType::Rooster:
-      return "rooster";
-    case CardType::Tiger:
-      return "tiger";
-
-    default:
-      const size_t cardNum = (size_t)card;
-      throw std::runtime_error(std::format("Invalid card type {}", cardNum));
-  }
-}
 
 bool Card::HasMove(const Offset offset) const {
   const std::vector moves = GetMoves();
@@ -131,7 +13,7 @@ bool Card::HasMove(const Offset offset) const {
 
 std::ostream& Card::StreamRow(std::ostream& stream, const int8_t row,
                               const bool rotate) const {
-  const std::vector<Offset> offsets = GetMoves();
+  const std::vector<Offset>& offsets = GetMoves();
   const int8_t sign = rotate ? -1 : 1;
 
   for (Offset offset{.dx = (int8_t)(-2 * sign), .dy = row}; abs(offset.dx) <= 2;
--- src/game/card.h
+++ src/game/card.h
@@ -2,6 +2,7 @@
 
 #include <ostream>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include "../util/offset.h"
@@ -29,14 +30,100 @@ enum class CardType {
   CardTypeCount,
 };
 
-struct Card {
-  static std::vector<Offset> GetMoves(const CardType card);
-  static Color GetColor(const CardType card);
-  static std::string GetName(const CardType card);
+const static inline std::vector<Offset> BoarMoves{Offset{-1, 0}, Offset{0, -1},
+                                                  Offset{1, 0}};
+const static inline std::vector<Offset> CobraMoves{Offset{-1, 0}, Offset{1, -1},
+                                                   Offset{1, 1}};
+const static inline std::vector<Offset> CrabMoves{Offset{-2, 0}, Offset{0, -1},
+                                                  Offset{2, 0}};
+const static inline std::vector<Offset> CraneMoves{Offset{-1, 1}, Offset{0, -1},
+                                                   Offset{1, 1}};
+const static inline std::vector<Offset> DragonMoves{
+    Offset{-2, -1}, Offset{-1, 1}, Offset{1, 1}, Offset{2, -1}};
+const static inline std::vector<Offset> EelMoves{Offset{-1, -1}, Offset{-1, 1},
+                                                 Offset{1, 0}};
+const static inline std::vector<Offset> ElephantMoves{
+    Offset{-1, -1}, Offset{-1, 0}, Offset{1, -1}, Offset{1, 0}};
+const static inline std::vector<Offset> FrogMoves{Offset{-2, 0}, Offset{-1, -1},
+                                                  Offset{1, 1}};
+const static inline std::vector<Offset> GooseMoves{
+    Offset{-1, -1}, Offset{-1, 0}, Offset{1, 0}, Offset{1, 1}};
+const static inline std::vector<Offset> HorseMoves{Offset{-1, 0}, Offset{0, -1},
+                                                   Offset{0, 1}};
+const static inline std::vector<Offset> MantisMoves{
+    Offset{-1, -1}, Offset{0, 1}, Offset{1, -1}};
+const static inline std::vector<Offset> MonkeyMoves{
+    Offset{-1, -1}, Offset{-1, 1}, Offset{1, -1}, Offset{1, 1}};
+const static inline std::vector<Offset> OxMoves{Offset{0, -1}, Offset{0, 1},
+                                                Offset{1, 0}};
+const static inline std::vector<Offset> RabbitMoves{
+    Offset{-1, 1}, Offset{1, -1}, Offset{2, 0}};
+const static inline std::vector<Offset> RoosterMoves{
+    Offset{-1, 0}, Offset{-1, 1}, Offset{1, 0}, Offset{1, -1}};
+const static inline std::vector<Offset> TigerMoves{Offset{0, -2}, Offset{0, 1}};
 
-  Color GetColor() const { return GetColor(Type); };
-  std::vector<Offset> GetMoves() const { return GetMoves(Type); };
-  std::string GetName() const { return GetName(Type); };
+const static inline std::unordered_map<CardType, const std::vector<Offset>&>
+    CardToMoves{
+        {CardType::Boar, BoarMoves},
+        {CardType::Cobra, CobraMoves},
+        {CardType::Crab, CrabMoves},
+        {CardType::Crane, CraneMoves},
+        {CardType::Dragon, DragonMoves},
+        {CardType::Eel, EelMoves},
+        {CardType::Elephant, ElephantMoves},
+        {CardType::Frog, FrogMoves},
+        {CardType::Goose, GooseMoves},
+        {CardType::Horse, HorseMoves},
+        {CardType::Mantis, MantisMoves},
+        {CardType::Monkey, MonkeyMoves},
+        {CardType::Ox, OxMoves},
+        {CardType::Rabbit, RabbitMoves},
+        {CardType::Rooster, RoosterMoves},
+        {CardType::Tiger, TigerMoves},
+    };
+
+const static inline std::unordered_map<CardType, Color> CardToColor{
+    {CardType::Boar, Color::Red},    {CardType::Cobra, Color::Red},
+    {CardType::Dragon, Color::Red},  {CardType::Elephant, Color::Red},
+    {CardType::Frog, Color::Red},    {CardType::Horse, Color::Red},
+    {CardType::Mantis, Color::Red},  {CardType::Rooster, Color::Red},
+    {CardType::Crab, Color::Blue},   {CardType::Crane, Color::Blue},
+    {CardType::Eel, Color::Blue},    {CardType::Goose, Color::Blue},
+    {CardType::Monkey, Color::Blue}, {CardType::Ox, Color::Blue},
+    {CardType::Rabbit, Color::Blue}, {CardType::Tiger, Color::Blue},
+};
+
+const static inline std::unordered_map<CardType, std::string> CardToString{
+    {CardType::Boar, "boar"},
+    {CardType::Cobra, "cobra"},
+    {CardType::Crab, "crab"},
+    {CardType::Crane, "crane"},
+    {CardType::Dragon, "dragon"},
+    {CardType::Eel, "eel"},
+    {CardType::Elephant, "elephant"},
+    {CardType::Frog, "frog"},
+    {CardType::Goose, "goose"},
+    {CardType::Horse, "horse"},
+    {CardType::Mantis, "mantis"},
+    {CardType::Monkey, "monkey"},
+    {CardType::Ox, "ox"},
+    {CardType::Rabbit, "rabbit"},
+    {CardType::Rooster, "rooster"},
+    {CardType::Tiger, "tiger"},
+};
+
+struct Card {
+  static const std::vector<Offset>& GetMoves(const CardType card) {
+    return CardToMoves.at(card);
+  }
+  static Color GetColor(const CardType card) { return CardToColor.at(card); }
+  static std::string GetName(const CardType card) {
+    return CardToString.at(card);
+  }
+
+  Color GetColor() const { return GetColor(Type); }
+  const std::vector<Offset>& GetMoves() const { return GetMoves(Type); }
+  std::string GetName() const { return GetName(Type); }
   bool HasMove(const Offset offset) const;
 
   bool operator==(const Card& other) const { return Type == other.Type; }
